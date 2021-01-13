b89a8146f9f4e10b2cb9c62a114f059d54514ff7
Michael Maggs <MichaelNMaggs@users.noreply.github.com>
2021-01-13 16:10:39 +0000

    Minor

--- scratch.py
+++ scratch.py
@@ -1,17 +1,1 @@
 
-
-import pywikibot
-
-
-
-article = 'Father Christmas'
-
-
-wikipedia = pywikibot.Site('en', 'wikipedia')
-print(wikipedia)
-page = pywikibot.Page(wikipedia, article)
-username = pywikibot.config.usernames['wikipedia']['en']
-
-text = page.text
-
-
